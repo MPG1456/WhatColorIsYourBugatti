805ca498034fb5c420851df78a7870934a12062b
MPG1456 <pc.lenovo.mpg@gmail>
2025-05-02 19:08:29 +0330

    Added changes to vehicle abstract class.

--- vehicle.cpp
+++ vehicle.cpp
@@ -1,7 +1,12 @@
 #include "vehicle.h"
 
-Vehicle::Vehicle(int maxS, bool iR, bool iS) : maxSpeed(maxS), isRent(iR), isSell(iS)
-{}
+int Vehicle::counter = 0;
+
+Vehicle::Vehicle(int maxS, bool iR, bool iS, string company, int model, string color, int YOP) : maxSpeed(maxS), isRent(iR), isSell(iS), company(company), model(model), color(color), yearOfProduction(YOP)
+{
+    availble = true;
+    id = ++counter;
+}
 
 int Vehicle::getMaxSpeed(void) const
 {
@@ -16,5 +21,35 @@ bool Vehicle::getRent(void) const
 bool Vehicle::getSell(void) const
 {
     return isSell;
+}
+
+string Vehicle::getCompanyName(void) const
+{
+    return company;
+}
+
+int Vehicle::getModel(void) const
+{
+    return model;
+}
+
+string Vehicle::getColor(void) const
+{
+    return color;
+}
+
+int Vehicle::getYOP(void) const
+{
+    return yearOfProduction;
+}
+
+bool Vehicle::getAvailable(void) const
+{
+    return availble;
+}
+
+void Vehicle::setAvailable(bool status)
+{
+    availble = status;
 }
 
--- vehicle.h
+++ vehicle.h
@@ -6,16 +6,29 @@ using namespace std;
 
 class Vehicle
 {
+    int id;
     int maxSpeed;
     bool isRent;
     bool isSell;
-
+    string company;
+    int model;
+    string color;
+    int yearOfProduction;
+    bool availble;
+    
 public:
-    Vehicle(int maxSpeed, bool isRent, bool isSell);
+    static int counter;
+    Vehicle(int maxSpeed, bool isRent, bool isSell, string company, int model, string color, int YOP);
     int getMaxSpeed(void) const;
     bool getRent(void) const;
     bool getSell(void) const;
+    string getCompanyName(void) const;
+    int getModel(void) const;
+    string getColor(void) const;
+    int getYOP(void) const;
     virtual void drive(void) const = 0;
     virtual void showFeatures(void) const = 0;
+    bool getAvailable(void) const;
+    void setAvailable(bool status);
 };
 
